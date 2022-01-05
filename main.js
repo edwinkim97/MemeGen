5a6facfa8d81d8f8a541569b3116cae000053510
edwinkim97 <71800237+edwinkim97@users.noreply.github.com>
2022-01-05 03:16:11 -0800

    Updated formatting on JS

--- main.js
+++ main.js
@@ -15,24 +15,24 @@ meme.addEventListener("submit", function(e){
     
     var del=document.createElement('button');
     del.textContent="Delete";
-    del.setAttribute('id','del')
-
-	var imgUrl=document.createElement("img");
-	imgUrl.src=document.getElementById("img-url").value;
+    del.setAttribute('id','del');
 	
-	var newMeme=document.createElement("li");
+    var imgUrl=document.createElement("img");
+    imgUrl.src=document.getElementById("img-url").value;
+	
+    var newMeme=document.createElement("li");
     memes.appendChild(newMeme);
 
     e.preventDefault();
 
-	newMeme.appendChild(imgUrl);
+    newMeme.appendChild(imgUrl);
     newMeme.appendChild(up);
-	newMeme.appendChild(down);
-	newMeme.appendChild(del);
+    newMeme.appendChild(down);
+    newMeme.appendChild(del);
 
     del.addEventListener('click', function(){
-		newMeme.remove();
-	})
+	newMeme.remove();
+    })
 
     meme.reset();
 });
